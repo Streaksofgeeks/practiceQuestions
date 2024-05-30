2b968f8d3fe61c60302cd68658b12712eb52eade
Streaksofgeeks <santpurush69@gmail.com>
2024-05-30 06:49:01 -0700

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class 1{
+    public static void golgappa(int n){
+    
+    for (int i = 0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (i==0||j==0||i==n-1||j==n-1){
+                System.out.print("*");
+            } else{
+                System.out.print(" ");
+            }
+        } System.out.println();
+    } 
+    }
+    public static void main(String[] args){
+        int n= 5;
+        golgappa(n);
+        
+    }
+
+}
