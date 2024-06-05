41d68e4b274ea206cbdbdb41fd2b3efd83a79964
Streaksofgeeks <santpurush69@gmail.com>
2024-06-04 23:21:50 -0700

    Create Question7.java

---
+++ Question7.java
@@ -0,0 +1,16 @@
+// WAP to add two numbers
+import java.util.*;
+public class Question7 {
+    public static void sum(){
+        Scanner scan = new Scanner (System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int sum = 0;
+        System.out.println(sum = a+b);
+
+    }
+    public static void main(String args[]){
+        sum();
+    }
+    
+}
